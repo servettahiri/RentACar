664856e000abdeeef9954f73f93abe6edbb5a8c2
miloskrga <miloskrga1998sd@gmail.com>
2023-07-31 15:30:04 +0200

    Prvo dodavanje

---
+++ RentACar_V01/Controller/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACar_V01.Controller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RentACar_V01/Model/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace RentACar_V01.Model
+{
+    public class Car
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int Kubikaza { get; set; }
+        public int Snaga { get; set; }
+    }
+}
